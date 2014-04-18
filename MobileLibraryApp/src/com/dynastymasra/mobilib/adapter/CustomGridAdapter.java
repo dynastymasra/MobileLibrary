3235f72bce58852351107f17e9bb4c547a1e3f29
dynastymasra <dynastymasra@gmail.com>
2014-04-18 18:22:20 +0700

    grid layout

---
+++ MobileLibraryApp/src/com/dynastymasra/mobilib/adapter/CustomGridAdapter.java
@@ -0,0 +1,47 @@
+package com.dynastymasra.mobilib.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.dynastymasra.mobilib.R;
+
+/**
+ * Author   : Dynastymasra
+ * Name     : Dimas Ragil T
+ * Email    : dynastymasra@gmail.com
+ * LinkedIn : http://www.linkedin.com/in/dynastymasra
+ * Blogspot : dynastymasra.wordpress.com | dynastymasra.blogspot.com
+ */
+public class CustomGridAdapter extends ArrayAdapter<String> {
+    private Context context;
+    private Integer[] images;
+    private String[] text;
+
+    public CustomGridAdapter(Context context, Integer[] images, String[] text) {
+        super(context, R.layout.fragment_main, text);
+        this.context = context;
+        this.images = images;
+        this.text = text;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.custom_grid_layout, parent, false);
+        }
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.homeGridImage);
+        TextView textView = (TextView) convertView.findViewById(R.id.homeGridText);
+
+        imageView.setImageResource(images[position]);
+        textView.setText(text[position]);
+
+        return convertView;
+    }
+}
--- MobileLibraryApp/src/com/dynastymasra/mobilib/fragment/MainFragment.java
+++ MobileLibraryApp/src/com/dynastymasra/mobilib/fragment/MainFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import com.dynastymasra.mobilib.R;
+import com.dynastymasra.mobilib.adapter.CustomGridAdapter;
 
 /**
  * Author   : Dynastymasra
@@ -15,10 +17,17 @@ import com.dynastymasra.mobilib.R;
  * Blogspot : dynastymasra.wordpress.com | dynastymasra.blogspot.com
  */
 public class MainFragment extends Fragment {
+    private GridView gridLayout;
+    private Integer[] images = {R.drawable.ic_booked, R.drawable.ic_cd};
+    private String[] text = {"Text 1", "Text 2"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        gridLayout = (GridView) view.findViewById(R.id.girdViewHome);
+        CustomGridAdapter customGridAdapter = new CustomGridAdapter(getActivity(), images, text);
+        gridLayout.setAdapter(customGridAdapter);
 
         return view;
     }
